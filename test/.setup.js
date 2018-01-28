40d98cc92c2116312ee29279d83a7a8946ff0bd4
Dave Honneffer <pearofducks@gmail.com>
2018-01-28 09:19:41 +0100

    Bump test-deps

--- test/.setup.js
+++ test/.setup.js
@@ -1,16 +1,1 @@
-// from - https://github.com/zinserjan/mocha-webpack/blob/master/docs/guides/jsdom.md
-const jsdom = require('jsdom').jsdom;
-
-global.document = jsdom('');
-global.window = document.defaultView;
-window.console = global.console;
-
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    global[property] = document.defaultView[property];
-  }
-});
-
-global.navigator = {
-  userAgent: 'node.js'
-};
+require('jsdom-global')()
